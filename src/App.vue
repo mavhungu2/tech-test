f7ad97e3a39650d30089bae8aa537d665693ee19
Isaac Mpharalala <mavhungu2@gmail.com>
2019-08-23 16:51:58 +0200

    Added Bootstrap Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
+    <Navigation></Navigation>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import Navigation from './components/NavBar.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
+    Navigation
   },
 };
 </script>
@@ -23,6 +27,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+ <div>
+  <b-navbar toggleable="sm" type="dark" variant="info">
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Home</b-nav-item>
+        <b-nav-item href="#">About</b-nav-item>
+        <b-nav-item href="#">Task</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation'
+};
+</script>
