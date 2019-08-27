bdb5dc3ac67b1b49adfb62598b17a55c7b26ce6b
Isaac Mpharalala <mavhungu2@gmail.com>
2019-08-27 11:06:44 +0200

    User vuex for task number 6

--- src/store.js
+++ src/store.js
@@ -1,16 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    currencies: [],
+    conversed_value: ""
   },
   mutations: {
-
-  },
-  actions: {
-
-  },
+    get_currencies (state) {
+      axios.get("https://api.exchangeratesapi.io/latest").then(Response => {
+        for(var item in Response['data']['rates']){
+          state.currencies.push(item)
+        }
+      })
+    },
+    convert_value(state, payload){
+      axios.get("https://free.currconv.com/api/v7/convert?q="+payload['conversion']+"&compact=ultra&apiKey=b8722ac32c4333c77884").then(Response=>{
+          state.conversed_value =  payload['amount']+ " "+ payload['currency'] + " = " + (Response['data'][payload['conversion']]*payload['amount']) +" ZAR"
+      })
+    }
+  }
 });
